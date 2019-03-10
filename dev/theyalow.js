ef0878f069599ff8596aec5ed4b08ca5885f6310
Anton Golovko <47984717+antonionick@users.noreply.github.com>
2019-03-10 14:00:18 +0300

    init: start theyalow task

---
+++ dev/theyalow.js
@@ -0,0 +1,2 @@
+import "./theyalow.html";
+import "./scss/theyalow.scss";
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: "./dev/theyalow.js",
+  },
+  output: {
+    filename: "theyalow.js",
+    publicPath: "dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          "postcss-loader",
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              filename: '[name].[ext]',
+              outputPath: 'assets/images',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              filename: '[name].[ext]',
+              outputPath: 'assets/fonts',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].html',
+        }
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'theyalow.css',
+    }),
+  ],
+  devServer: {
+    open: true,
+    overlay: true,
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
+};
